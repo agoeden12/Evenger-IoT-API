90ab9fc10fbed098f27145afae4e4b8b407b8fb8
Andrew Goeden <andrewgoeden@gmail.com>
2021-04-02 00:00:26 -0400

    fixed template

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/swag-shop');
-var createError = require('http-errors');
+var mongoose = require("mongoose");
+// var db = mongoose.connect('mongodb://localhost/swag-shop');
+var createError = require("http-errors");
 
-var Session = require('./schema/Session');
-var Point = require('./schema/Point');
+var Session = require("./schema/Session");
+var Point = require("./schema/Point");
 
-app.all('/*', function(req, res, next) {
+app.all("/*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, Content-Type, Accept"
+  );
   res.header("Access-Control-Allow-Methods", "POST, GET");
   next();
 });
@@ -17,12 +20,22 @@ app.all('/*', function(req, res, next) {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(function(req, res, next) {
+app.get("/test", (req, res) => {
+  res
+    .status(200)
+    .json({
+      title: "Test Working",
+      body: "the template is good so far",
+    })
+    .send();
+});
+
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.listen(5000, function() {
-    console.log("Voltron API running on port 5000...");
+app.listen(5000, function () {
+  console.log("Voltron API running on port 5000...");
 });
 
 module.exports = app;
